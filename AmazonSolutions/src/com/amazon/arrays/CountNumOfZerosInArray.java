0e4cb8f770a86be0eee561ef6b4cce1598eaff7c
akkhil2012 <akkhil2012@gmail.com>
2015-12-08 22:23:43 +0530

    search count of zeros in O(logn)

---
+++ AmazonSolutions/src/com/amazon/arrays/CountNumOfZerosInArray.java
@@ -0,0 +1,27 @@
+package com.amazon.arrays;
+
+
+public class CountNumOfZerosInArray {
+	public static int countZeros(int[] arr){
+		int count = 0;
+		int low = 0; int mid;
+		int high = arr.length-1;
+		while(low<=high){
+			mid = (high+low)/2;
+			if(arr[mid]<1){
+				low = mid+1;
+			}else{
+				high = mid-1;
+			}
+		}
+		return low;
+	}
+	
+	public static void main(String args[]) {
+	
+		int [] arr = {0,0,0,0,1,1};
+		System.out.println(" Number of zeros is "+ new CountNumOfZerosInArray().countZeros(arr));
+		
+		
+	}
+}
