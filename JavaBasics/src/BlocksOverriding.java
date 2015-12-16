6e0b8a290413168b1d9ffb223a5bb44e10811e1a
akkhil2012 <akkhil2012@gmail.com>
2015-12-16 14:35:39 +0530

    KnapSack

--- JavaBasics/src/BlocksOverriding.java
+++ JavaBasics/src/BlocksOverriding.java
@@ -1,46 +1,40 @@
 
-
-
-class SuperClass{
-	static{
+class SuperClass {
+	static {
 		System.out.println(" Inside super static");
 	}
 	{
 		System.out.println(" Inside super");
 	}
+
 	public SuperClass() {
 		System.out.println(" Inside super constructor ");
 	}
 }
 
-
-
-class SubClass extends SuperClass{
-static{
-	System.out.println(" Inside sub static");
+class SubClass extends SuperClass {
+	static {
+		System.out.println(" Inside sub static");
 	}
-	
+
 	{
 		System.out.println(" Inside sub");
 	}
-	
+
 	public SubClass() {
 		System.out.println(" Inside sub constructor ");
 	}
-	
+
 }
 
-
 public class BlocksOverriding {
-	
-	  public static void main(String args[]) {
-		
-		  new SuperClass();
-		  
-		  new SubClass();
-		  
+
+	public static void main(String args[]) {
+
+		new SuperClass();
+
+		new SubClass();
+
 	}
-	
-	
 
 }
---
+++ JavaBasics/src/ExecutorServiceSample.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+class ExecutorServiceTest{
+	
+	
+	ExecutorService executor = Executors.newFixedThreadPool(2);
+	
+	//Runnable taskOne = new MyThread();
+	
+	
+}
+
+
+
+
+
+public class ExecutorServiceSample {
+
+}
